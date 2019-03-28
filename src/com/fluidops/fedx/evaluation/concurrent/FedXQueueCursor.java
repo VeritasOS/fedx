40068f0488a9967b24f2dbd91853538cd0351aaa
Andreas Schwarte <andreas.schwarte@veritas.com>
2019-03-28 13:32:56 +0100

    #16: make sure to properly close non-consumed iterations
    The QueueCursor might have iterations that are not properly closed. This
    may happend if the query result is not consumed entirely.
    
    This change makes sure to properly close any left-over (non-consumed)
    iteration

--- src/com/fluidops/fedx/evaluation/concurrent/FedXQueueCursor.java
+++ src/com/fluidops/fedx/evaluation/concurrent/FedXQueueCursor.java
@@ -3,6 +3,8 @@ package com.fluidops.fedx.evaluation.concurrent;
 import org.eclipse.rdf4j.common.iteration.CloseableIteration;
 import org.eclipse.rdf4j.query.QueryEvaluationException;
 import org.eclipse.rdf4j.query.impl.QueueCursor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Specialized variants of {@link QueueCursor} which avoids converting any
@@ -15,6 +17,8 @@ import org.eclipse.rdf4j.query.impl.QueueCursor;
  */
 public class FedXQueueCursor<T> extends QueueCursor<CloseableIteration<T, QueryEvaluationException>> {
 
+	private static final Logger log = LoggerFactory.getLogger(FedXQueueCursor.class);
+
 	public FedXQueueCursor(int capacity) {
 		super(capacity);
 	}
@@ -25,6 +29,27 @@ public class FedXQueueCursor<T> extends QueueCursor<CloseableIteration<T, QueryE
 			return (QueryEvaluationException) e;
 		}
 		return super.convert(e);
+	}
+
+	@Override
+	public void handleClose() throws QueryEvaluationException {
+		done();
+
+		try {
+			// consume all remaining elements from the queue
+			while (hasNext()) {
+				try {
+					log.trace("Attempting to close non consumed inner iteration.");
+					CloseableIteration<T, QueryEvaluationException> closable = next();
+					closable.close();
+				} catch (Exception e) {
+					log.debug("Failed to closed inner iteration: " + e.getMessage());
+					log.trace("Details: ", e);
+				}
+			}
+		} finally {
+			super.handleClose();
+		}
 	}
 
 }
